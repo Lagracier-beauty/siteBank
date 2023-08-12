7913cdd3c1f72a7423848257f032459c75f0a83d
lili <lili@gmail.com>
2023-08-12 14:08:16 +0000

    commit everything

---
+++ index.js
@@ -0,0 +1,11 @@
+let btn = querySelector("btn");
+
+
+let myBtn = document.querySelector(".btn");
+myBtn.style.backgroundColor = "white";
+myBtn.style.color = "blue";
+myBtn.style.border = " 2px solid blue";
+myBtn.style.width = "16vh";
+myBtn.style.height = "4vh";
+myBtn.style.borderRadius = "5vh";
+myBtn.style.cursor = "pointer";
