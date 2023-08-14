882c4511742865d62836f47b43262713e084db99
lili <lili@gmail.com>
2023-08-14 09:14:25 +0000

    tout les changements

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-let btn = querySelector("btn");
+console.log( "my lovely God");
 
 
-let myBtn = document.querySelector(".btn");
-myBtn.style.backgroundColor = "white";
-myBtn.style.color = "blue";
-myBtn.style.border = " 2px solid blue";
-myBtn.style.width = "16vh";
-myBtn.style.height = "4vh";
-myBtn.style.borderRadius = "5vh";
-myBtn.style.cursor = "pointer";
+
